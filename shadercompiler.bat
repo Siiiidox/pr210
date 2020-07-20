942fc768abd3220d29ed1878e78bc205ed363fe5
Kay Hennig <KayHennig98@gmail.com>
2020-07-20 10:49:05 +0200

    [*] compiler creates folder if not exsist

--- shadercompiler.bat
+++ shadercompiler.bat
@@ -1,2 +1,5 @@
+IF NOT EXIST "bin\data" md bin\data
+IF NOT EXIST "bin\data\shd" md bin\data\shd
+
 for %%f in (vs_*.hlsl) do fxc /T vs_5_0 /E VS_Main /Fo "bin/data/shd/%%~nf.shader" "%%~f"
 for %%f in (ps_*.hlsl) do fxc /T ps_5_0 /E PS_Main /Fo "bin/data/shd/%%~nf.shader" "%%~f"
